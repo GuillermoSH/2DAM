7d7ce7fc63d0b8cd7f347d36b400133c843be6ff
GuillermoSH <siciliahernandezguillermo@gmail.com>
2026-01-23 20:29:10 +0000

    :construction: Subeindo codigo

--- DAD/apps_pruebas/cuestionario_repaso/test/widget_test.dart
+++ DAD/apps_pruebas/cuestionario_repaso/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cuestionario_repaso/main.dart';
+import 'package:cuestionario_repaso/ejercicio_2_1.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
